adcfaa35d1ec3546b4d2a2d4494bc01c0cc63983
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-06-17 10:08:59 -0400

    Revert "Use "///" instead of "/" when loading main.qml"
    This reverts commit 5b1db19a47192b78bb49f773b213ca5c29e2f6eb.

--- simple/src/main.cpp
+++ simple/src/main.cpp
@@ -6,8 +6,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.addImportPath(":/.");
-    engine.load(QUrl("qrc:///qml/main.qml"));
+    engine.load(QUrl("qrc:/qml/main.qml"));
 
     return app.exec();
 }
