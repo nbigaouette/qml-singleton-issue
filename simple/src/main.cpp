5b1db19a47192b78bb49f773b213ca5c29e2f6eb
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-06-17 10:03:11 -0400

    Use "///" instead of "/" when loading main.qml

--- simple/src/main.cpp
+++ simple/src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl("qrc:/qml/main.qml"));
+    engine.load(QUrl("qrc:///qml/main.qml"));
 
     return app.exec();
 }
